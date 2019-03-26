852fdb6142d6e887f282a0ad101bb7aae8c36f36
josedgregorio <josedgregorio07@gmail.com>
2019-03-26 13:57:29 -0600

    cambios visuales

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -4,24 +4,24 @@
     <div class="row">
         <div class="col-md-12">
 			<div class="row div-margin">
-				<div class="col-md-6 col-xs-6">
-					<a class="div-padding" href="{{ backpack_url('clientes') }}">
+				<div class="col-md-6 col-xs-6 div-padding">
+					<a class="" href="{{ backpack_url('clientes') }}">
 						<div class="div-item-menu">
 							<div class="div-icon" style="background-image: url('/img/clientes.png');"></div>
 							<h1 class="title-icon">Clientes</h1>
 						</div>
 					</a>
 				</div>
-				<div class="col-md-6 col-xs-6">
-					<a class="div-padding" href="{{ backpack_url('productos') }}">
+				<div class="col-md-6 col-xs-6 div-padding">
+					<a class="" href="{{ backpack_url('productos') }}">
 						<div class="div-item-menu">
 							<div class="div-icon" style="background-image: url('/img/producto.png');"></div>
 							<h1 class="title-icon">Productos</h1>
 						</div>
 					</a>
 				</div>
-				<div class="col-md-6 col-xs-6">
-					<a class="div-padding" href="{{ backpack_url('pedidos') }}" id="link-pedido">
+				<div class="col-md-6 col-xs-6 div-padding">
+					<a class="" href="{{ backpack_url('pedidos') }}" id="link-pedido">
 						<div class="div-item-menu">
 							<div class="div-icon" style="background-image: url('/img/pedido.png');"></div>
 							<h1 class="title-icon">Pedidos</h1>
@@ -29,8 +29,8 @@
 					</a>
 				</div>
 				@if(Backpack_auth()->user()->tipo_user == 1)
-					<div class="col-md-6 col-xs-6">
-						<a class="div-padding" href="{{ backpack_url('user') }}">
+					<div class="col-md-6 col-xs-6 div-padding">
+						<a class="" href="{{ backpack_url('user') }}">
 							<div class="div-item-menu">
 								<div class="div-icon" style="background-image: url('/img/usuarios.png');"></div>
 								<h1 class="title-icon">Usuarios</h1>
@@ -38,8 +38,8 @@
 						</a>
 					</div>
 				@endif
-				<div class="col-md-6 col-xs-6">
-					<a class="div-padding" href="{{ url(config('backpack.base.route_prefix', 'admin').'/logout') }}">
+				<div class="col-md-6 col-xs-6 div-padding">
+					<a class="" href="{{ url(config('backpack.base.route_prefix', 'admin').'/logout') }}">
 						<div class="div-item-menu">
 							<div class="div-icon" style="background-image: url('/img/salir.png');"></div>
 							<h1 class="title-icon">Salir</h1>
--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,7 @@
 	Route::get('crear-permisos/','PermissionCrudController@crear_permisos');
 	Route::get('clientes/{id}','CustomerCrudController@get_customer');
 	Route::get('cliente/saldo/{id}','CustomerCrudController@get_saldo');
-	Route::get('productos/{id}','ItemCrudController@get_item');
+	Route::get('producto/{id}','ItemCrudController@get_item');
 	Route::get('productos/todos','ItemCrudController@get_items');
 
 	CRUD::resource('clientes', 'CustomerCrudController');
