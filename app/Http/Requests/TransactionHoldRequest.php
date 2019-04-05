178ec32c33b855a174de31f571fa657961306284
Fermin Arellano <ferminarellano.hn@gmail.com>
2019-04-05 12:09:49 -0600

    Validaciones faltantes del formulario de Pedido.

--- app/Http/Requests/TransactionHoldRequest.php
+++ app/Http/Requests/TransactionHoldRequest.php
@@ -7,23 +7,29 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TransactionHoldRequest extends FormRequest
 {
-    public function authorize()
-    {
-        return backpack_auth()->check();
-    }
+	public function authorize()
+	{
+		return backpack_auth()->check();
+	}
 
-    public function rules()
-    {
-        return [];
-    }
+	public function rules()
+	{
+		return [
+			'productos' => 'required',
+			'CustomerID' => 'required',
+		];
+	}
 
-    public function attributes()
-    {
-        return [];
-    }
+	public function attributes()
+	{
+		return [];
+	}
 
-    public function messages()
-    {
-        return [];
-    }
+	public function messages()
+	{
+		return [
+			'productos.required' => 'Necesita seleccionar al menos un producto.',
+			'CustomerID.required' => 'Necesita seleccionar el cliente.'
+		];
+	}
 }
